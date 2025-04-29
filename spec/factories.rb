fe9512e666e4ee08005831ee062c035e02dbe0ee
Carl Fink <cafink@gmail.com>
2025-04-29 09:23:03 -0500

    Create friends CRUD app

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,10 @@
 FactoryBot.define do
+  factory :friend do
+    first_name { "MyString" }
+    last_name { "MyString" }
+    email { "MyString" }
+    phone { "MyString" }
+    twitter { "MyString" }
+  end
+
 end
